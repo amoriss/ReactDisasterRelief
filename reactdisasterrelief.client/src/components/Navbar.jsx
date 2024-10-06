c1fa4e94f2a5dc10cec58c2dff6e0653175a4475
Amoriss <amoriss@protonmail.com>
2024-10-06 18:14:55 -0500

    nav bar with home and gethelp pages working

---
+++ reactdisasterrelief.client/src/components/Navbar.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <nav style={styles.navbar}>
+            <h1 style={styles.title}>Disaster Relief</h1>
+            <ul style={styles.navlist}>
+                <li style={styles.navItem}>
+                    <Link to="/" style={styles.navLink}>Home</Link>
+                </li>
+                <li style={styles.navItem}>
+                    <Link to="/get-help" style={styles.navLink}> Get Help</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+const styles = {
+    navbar: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '10px 20px',
+        backgroundColor: 'black',
+        color: 'white',
+    },
+    title: {
+        margin: 0,
+    },
+    navList: {
+        listStyle: 'none',
+        display: 'flex',
+        margin: '0',
+        padding: '0',
+    },
+    navItem: {
+        marginLeft: '20px',
+    },
+    navLink: {
+        color: 'white',
+        textDecoration: 'none',
+    },
+};
+
+export default Navbar;
